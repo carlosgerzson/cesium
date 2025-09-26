7f275de2fc68f79ac51e19ce430f56299d748c60
carlosgerzson <151975728+carlosgerzson@users.noreply.github.com>
2025-09-26 19:28:47 -0300

    Add files via upload

--- buildingsData.js
+++ buildingsData.js
@@ -1,147 +1,267 @@
 // Dados para os prédios que terão alternância
 export const buildingsMap = {
-'505354016': { // Barão
-position: Cesium.Cartesian3.fromDegrees(-54.316222, -30.3411985, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/barao.glb',
-},
-'505354014': { // Corsan
-position: Cesium.Cartesian3.fromDegrees(-54.316286, -30.341502, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/corsan.glb',
-},
-'496696965': { // Banco da Província
-position: Cesium.Cartesian3.fromDegrees(-54.317309, -30.339836, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/provincia.glb',
-},
-'505354011': { // Banco do Brasil
-position: Cesium.Cartesian3.fromDegrees(-54.316374, -30.341547, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/bb.glb',
-},
-'228625933': { // Banrisul
-position: Cesium.Cartesian3.fromDegrees(-54.318080, -30.341116, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/banrisul.glb',
-},
-'496697002': { // Copex / Casa Central
-position: Cesium.Cartesian3.fromDegrees(-54.316500, -30.340160, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/copex.glb',
-},
-'496680317': { // Loja São Miguel
-position: Cesium.Cartesian3.fromDegrees(-54.317553, -30.340040, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/saomiguel.glb',
-},
-'496696981': { // Maçonaria
-position: Cesium.Cartesian3.fromDegrees(-54.317040, -30.339852, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/maçonaria.glb',
-},
-'496697003': { // Sobrado/biblioteca/D.Pedro
-position: Cesium.Cartesian3.fromDegrees(-54.316098, -30.340311, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/sobrado.glb',
-},
-'496692924': { // Casa onde Presidente Getúlio discursou
-position: Cesium.Cartesian3.fromDegrees(-54.316817, -30.341718, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/getulio.glb',
-},
-'496692932': { // Hoje Banco Bradesco
-position: Cesium.Cartesian3.fromDegrees(-54.317569, -30.341437, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/bradesco.glb',
-},
-'505354027': { // Casa América
-position: Cesium.Cartesian3.fromDegrees(-54.315992, -30.340752, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/america.glb',
-},
-'505354023': { // Casa do Pastor
-position: Cesium.Cartesian3.fromDegrees(-54.316104, -30.340986, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/pastor.glb',
-},
-'505353919': { // Casa família Terra
-position: Cesium.Cartesian3.fromDegrees(-54.315906, -30.340653, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/terra.glb',
-},
-'496680298': { // Casarão Amarelo
-position: Cesium.Cartesian3.fromDegrees(-54.317810, -30.340555, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/amarelo.glb',
-},
-'496692933': { // Clube Comercial
-position: Cesium.Cartesian3.fromDegrees(-54.317818, -30.341347, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/comercial.glb',
-},
-'505353915': { // Clube Guarani
-position: Cesium.Cartesian3.fromDegrees(-54.316168, -30.341109, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/guarani.glb',
-},
-'496680296': { // Café Central
-position: Cesium.Cartesian3.fromDegrees(-54.317857, -30.340649, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/cafecentral.glb',
-},
-'496696988': { // Farmácia Central
-position: Cesium.Cartesian3.fromDegrees(-54.316858, -30.340008, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/farmaciacentral.glb',
-},
-'496675629': { // Farmácia Confiança
-position: Cesium.Cartesian3.fromDegrees(-54.318204, -30.341344, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/farmaciaconfianca.glb',
-},
-'502141804': { // Igreja do Galo
-position: Cesium.Cartesian3.fromDegrees(-54.316628, -30.342041, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/galo.glb',
-},
-'496683075': { // Igreja Matriz
-position: Cesium.Cartesian3.fromDegrees(-54.317459, -30.341476, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/matriz.glb',
-},
-'505354033': { // Posto Esso 1944
-position: Cesium.Cartesian3.fromDegrees(-54.315853, -30.340659, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/esso.glb',
-},
-'494741651': { // Prefeitura Municipal
-position: Cesium.Cartesian3.fromDegrees(-54.319332, -30.339027, 0),
-elevationOffset: 0,
-orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
-uri_detail: 'models/prefeitura.glb',
-}
+    '505354016': { // Barão
+        position: Cesium.Cartesian3.fromDegrees(-54.316222, -30.3411985, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/barao.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505354016_1.jpg']
+        }
+    },
+    '505354014': { // Corsan
+        position: Cesium.Cartesian3.fromDegrees(-54.316286, -30.341502, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/corsan.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505354014_1.jpg']
+        }
+    },
+    '496696965': { // Banco da Província
+        position: Cesium.Cartesian3.fromDegrees(-54.317309, -30.339836, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/provincia.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496696965_1.jpg']
+        }
+    },
+    '505354011': { // Banco do Brasil
+        position: Cesium.Cartesian3.fromDegrees(-54.316374, -30.341547, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/bb.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505354011_1.jpg']
+        }
+    },
+    '228625933': { // Banrisul
+        position: Cesium.Cartesian3.fromDegrees(-54.318080, -30.341116, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/banrisul.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/228625933_1.jpg']
+        }
+    },
+    '496697002': { // Copex / Casa Central
+        position: Cesium.Cartesian3.fromDegrees(-54.316500, -30.340160, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/copex.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496697002_1.jpg']
+        }
+    },
+    '496680317': { // Loja São Miguel
+        position: Cesium.Cartesian3.fromDegrees(-54.317553, -30.340040, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/saomiguel.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496680317_1.jpg']
+        }
+    },
+    '496696981': { // Maçonaria
+        position: Cesium.Cartesian3.fromDegrees(-54.317040, -30.339852, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/maçonaria.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496696981_1.jpg']
+        }
+    },
+    '496697003': { // Sobrado/biblioteca/D.Pedro
+        position: Cesium.Cartesian3.fromDegrees(-54.316098, -30.340311, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/sobrado.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496697003_1.jpg']
+        }
+    },
+    '496692924': { // Casa onde Presidente Getúlio discursou
+        position: Cesium.Cartesian3.fromDegrees(-54.316817, -30.341718, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/getulio.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496692924_1.jpg']
+        }
+    },
+    '496692932': { // Hoje Banco Bradesco
+        position: Cesium.Cartesian3.fromDegrees(-54.317569, -30.341437, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/bradesco.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496692932_1.jpg']
+        }
+    },
+    '505354027': { // Casa América
+        position: Cesium.Cartesian3.fromDegrees(-54.315992, -30.340752, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/america.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505354027_1.jpg']
+        }
+    },
+    '505354023': { // Casa do Pastor
+        position: Cesium.Cartesian3.fromDegrees(-54.316104, -30.340986, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/pastor.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505354023_1.jpg']
+        }
+    },
+    '505353919': { // Casa família Terra
+        position: Cesium.Cartesian3.fromDegrees(-54.315906, -30.340653, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/terra.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505353919_1.jpg']
+        }
+    },
+    '496680298': { // Casarão Amarelo
+        position: Cesium.Cartesian3.fromDegrees(-54.317810, -30.340555, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/amarelo.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496680298_1.jpg']
+        }
+    },
+    '496692933': { // Clube Comercial
+        position: Cesium.Cartesian3.fromDegrees(-54.317818, -30.341347, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/comercial.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496692933_1.jpg']
+        }
+    },
+    '505353915': { // Clube Guarani
+        position: Cesium.Cartesian3.fromDegrees(-54.316168, -30.341109, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/guarani.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505353915_1.jpg']
+        }
+    },
+    '496680296': { // Café Central
+        position: Cesium.Cartesian3.fromDegrees(-54.317857, -30.340649, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/cafecentral.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496680296_1.jpg']
+        }
+    },
+    '496696988': { // Farmácia Central
+        position: Cesium.Cartesian3.fromDegrees(-54.316858, -30.340008, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/farmaciacentral.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496696988_1.jpg']
+        }
+    },
+    '496675629': { // Farmácia Confiança
+        position: Cesium.Cartesian3.fromDegrees(-54.318204, -30.341344, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/farmaciaconfianca.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496675629_1.jpg']
+        }
+    },
+    '502141804': { // Igreja do Galo
+        position: Cesium.Cartesian3.fromDegrees(-54.316628, -30.342041, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/galo.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/502141804_1.jpg']
+        }
+    },
+    '496683075': { // Igreja Matriz
+        position: Cesium.Cartesian3.fromDegrees(-54.317459, -30.341476, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/matriz.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/496683075_1.jpg']
+        }
+    },
+    '505354033': { // Posto Esso 1944
+        position: Cesium.Cartesian3.fromDegrees(-54.315853, -30.340659, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/esso.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/505354033_1.jpg']
+        }
+    },
+    '494741651': { // Prefeitura Municipal
+        position: Cesium.Cartesian3.fromDegrees(-54.319332, -30.339027, 0),
+        elevationOffset: 0,
+        orientation: new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(-156), Cesium.Math.toRadians(0), Cesium.Math.toRadians(0)),
+        uri_detail: 'models/prefeitura.glb',
+        info: {
+            title: 'Prédio histórico de São Gabriel',
+            description: 'Informações em breve.',
+            images: ['models/494741651_1.jpg']
+        }
+    }
 };
